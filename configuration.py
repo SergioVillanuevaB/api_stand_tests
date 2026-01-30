0230f63302d02599eea4080ca14dddfdf156f5ca
Sergio Villanueva Becerril <sergiovillanuevab99@gmail.com>
2026-01-29 20:33:44 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://cnt-16be3b9b-9fd6-49d3-a5df-24d0762126f3.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
